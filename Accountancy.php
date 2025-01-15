ec2ab316739839f656f399eed4a9de3a3ca718f8
KVM SANDEEPA <166363254+KVMSANDEEPA@users.noreply.github.com>
2025-01-15 13:23:11 +0530

    header
    add php include

--- Accountancy.php
+++ Accountancy.php
@@ -29,51 +29,7 @@
 </style>
 </head>
   <body>
-    <header>
-        <nav class="navbar navbar-expand-lg bg-body-tertiary p-4 rounded navbar-dark" aria-label="Thirteenth navbar example" style="background-color: rgb(15, 15, 57);">
-          <div class="container-fluid">
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample11" aria-controls="navbarsExample11" aria-expanded="false" aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon"></span>
-            </button>
-    
-            <div class="collapse navbar-collapse d-lg-flex" id="navbarsExample11">
-              <a class="navbar-brand col-lg-0 me-0" id="logo" href="index.html"><img src="assets/img/LOGOATI.png" width="80px"></a>
-              <ul class="navbar-nav col-lg-6 justify-content-lg-center">
-                <li class="nav-item">
-                  <a class="nav-link active" aria-current="page" href="index.html">Home</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="about.html">&nbsp;&nbsp;About</a>
-                </li>
-               
-                <li class="nav-item dropdown">
-                  <a class="nav-link dropdown-toggle" href="courses.html" id="dropdownId" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">&nbsp;&nbsp;Courses</a>
-                  <div class="dropdown-menu" aria-labelledby="dropdownId">
-                    <a class="dropdown-item p-3" href="courses.html">All Courses</a>
-                    <a class="dropdown-item p-3" href="IT.html">Information Technology</a>
-                    <a class="dropdown-item p-3" href="Engineering.html">Engineering</a>
-                    <a class="dropdown-item p-3" href="Accountancy.html">Accountancy</a>
-                    <a class="dropdown-item p-3" href="Agriculture.html">Agriculture</a>
-                    <a class="dropdown-item p-3" href="BA.html">Business Administration</a>
-                    <a class="dropdown-item p-3" href="English.html">English</a>
-                    <a class="dropdown-item p-3" href="Management.html">Management</a>
-                    <a class="dropdown-item p-3" href="QS.html">Quantity Survey</a>
-                    <a class="dropdown-item p-3" href="THM.html">Tourism and Hospitality Management</a>
-                  </div>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="contact.html">&nbsp;&nbsp;Contact </a>
-                </li>
-                
-          </ul>
-          <form class="form-inline my-2 my-lg-0">
-               <input class="form-control mr-sm-2" type="text" placeholder="Search">
-              <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-          </form>
-            </div>
-          </div>
-        </nav>
-      </header>
+  <?php include_once ("includes/header.php") ?>
     
     <section class="headingbg">
       <div class="heading">
